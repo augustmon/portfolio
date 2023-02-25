2d2292024f54dfa9839cff45a3567ab89f690406
augustmon <99360842+augustmon@users.noreply.github.com>
2023-02-25 12:08:24 +0100

    fixed: back arrow

--- src/pages/ProjectPage/ProjectPage.js
+++ src/pages/ProjectPage/ProjectPage.js
@@ -1,30 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./ProjectPage.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 //Components
 import TextBox from "../../components/TextBox/TextBox";
 
 export default function ProjectPage(props) {
+  //  if empty -> show this:
+  // UNDER CONSTRUCTION
+  const placeholder = (
+    <TextBox
+      title="UNDER CONSTRUCTION"
+      description="This project description is still under construction..."
+    />
+  );
+
   return (
     <div className="ProjectPage">
       <NavLink
         className="ProjectPage--backlink"
         to="/portfolio"
-        style={{ color: "black" }}
+        // style={{ color: "black" }}
       >
-        ⬅
+        <ArrowBackIcon fontSize="larger" />
       </NavLink>
-      <div className="ProjectPage--flex">
-        <TextBox
-          title={props.title}
-          description={props.description}
-          linkTitle={props.linkTitle}
-          href={props.href}
-        />
-        <img src={props.imgLeft} className="imgLeft" />
-        <img src={props.imgRight} className="imgRight" />
-      </div>
+      {props.title != undefined && (
+        <div className="ProjectPage--flex">
+          <TextBox
+            title={props.title}
+            description={props.description}
+            linkTitle={props.linkTitle}
+            href={props.href}
+          />
+          <img src={props.imgLeft} className="imgLeft" />
+          <img src={props.imgRight} className="imgRight" />
+        </div>
+      )}
+
+      {props.title == undefined && placeholder}
     </div>
   );
 }
